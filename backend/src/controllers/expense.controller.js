c751c55c53e2e66dd5f0d2dc703cba3a334c0247
Kei07r <kei.skillsboost@gmail.com>
2025-06-11 23:04:22 +0530

    Added controller mockups and finished routes page

---
+++ backend/src/controllers/expense.controller.js
@@ -0,0 +1,27 @@
+const addExpense = (req, res) => {
+  console.log("Add Expense Working");
+};
+
+const getExpense = (req, res) => {
+  console.log("Get Expense Working");
+};
+
+const scanExpense = (req, res) => {
+  console.log("Scan Bill Working");
+};
+
+const updateExpense = (req, res) => {
+  console.log("Update Expense Working");
+};
+
+const deleteExpense = (req, res) => {
+  console.log("Delete Expense Working");
+};
+
+module.exports = {
+  addExpense,
+  getExpense,
+  scanExpense,
+  updateExpense,
+  deleteExpense,
+};
--- backend/src/routes/expense.routes.js
+++ backend/src/routes/expense.routes.js
@@ -1,7 +1,13 @@
 const express = require("express");
 const router = express.Router();
 
-
+const {
+  addExpense,
+  getExpense,
+  scanExpense,
+  updateExpense,
+  deleteExpense,
+} = require("../controllers/expense.controller");
 
 router.post("/", addExpense);
 router.get("/", getExpense);
